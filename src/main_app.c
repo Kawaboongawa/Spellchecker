8b1c03b6b78465fd9be57bd24bb32c80abe2c847
Cyril CETRE <cyril.cetre@epita.fr>
2017-07-26 12:31:05 +0200

    TextMiningApp : add ref-like parsing

--- src/main_app.c
+++ src/main_app.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "levenshtein.h"
 #include "tools.h"
 
-int main()
-{
-    char* s1 = "coucou";
-    char* s2 = "coucuo";
-    printf("%d\n", levenshtein(s1, s2));
+#define BUF_SIZE 128
 
-    Word l[3] = {{.word = "blu", .freq = 52, .dist = 12},
-                 {.word = "bleu", .freq = 40, .dist = 42},
-                 {.word = "ble", .freq = 1, .dist = 3}};
-    print_json(l, 3);
-}
+
+
+int main(int argc, char** argv)
+{
+    /*Word l[3] = {{.word = "blu", .freq = 52, .dist = 12},
+      {.word = "bleu", .freq = 40, .dist = 42},
+      {.word = "ble", .freq = 1, .dist = 3}};
+      print_json(l, 3);*/
+    argv = argv;
+    //printf("%c\n", fgetc(stdin));
+    if (argc < 2)
+    {
+        printf("Usage: ./TextMiningApp /path/to/compiled/dict.bin\n");
+        printf("Aborted (core dumped)\n");
+        return 134;
+    }
+    
+    size_t  size = 0;
+    char* buf = NULL;
+    getline(&buf, &size, stdin);
+    printf("%s", buf);
+    free(buf);
+} 
+
+
